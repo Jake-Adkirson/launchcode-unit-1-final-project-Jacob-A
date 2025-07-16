76a3aa4e04ed0ba841f79424726765745ea6c307
Jake Adkirson <jikjaklol363@gmail.com>
2025-07-15 21:31:02 -0500

    added more details to profile page.

--- unit-1-final/src/components/UserProfile.jsx
+++ unit-1-final/src/components/UserProfile.jsx
@@ -1,7 +1,13 @@
 
 const UserProfile = () => {
     return(
+        <div>
         <img src="https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png?20150327203541" />
+        <p>Name:</p>
+        <p>Age:</p>
+        <p>State:</p>
+        <p>Preferred Watercraft:</p>
+        </div>
     );
 }
 
