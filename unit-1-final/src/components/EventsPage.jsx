770d89cc4a02cae0e2df73d090d95e52b2cf3ec7
Jake Adkirson <jikjaklol363@gmail.com>
2025-07-21 14:52:33 -0500

    used google sheets api to pull data from events sheet to publish on events page.

--- unit-1-final/src/components/EventsPage.jsx
+++ unit-1-final/src/components/EventsPage.jsx
@@ -1,15 +1,51 @@
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router';
+import axios from 'axios';
 
 const EventsPage = () => {
+    const [data, setData] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await axios.get(
+                    `https://sheets.googleapis.com/v4/spreadsheets/1XGZyeeLtsE3gUdgYZOBrXmymrHSWkzP9dFEiNwv5dBo/values/Sheet1!A2:D500?key=AIzaSyATPhbEPCa76G2XT63QCG0mpk4Q_BYYimI`);
+                    setData(response.data.values);
+                    setLoading(false);
+            } catch (error) {
+                setError(error);
+                setLoading(false);
+            }
+        }
+        fetchData();
+    }, []);
     
+    if(loading) {
+        return <p>Loading events...</p>
+    }
+
+    if (error) {
+        alert(error.message);
+    }
+
     return(
         <div className="EventsPage">
             <h2>Events</h2>
             <button>
                 <Link to="/events_page/create_event">Create Event</Link>
             </button>
-            <br/>
-            <p>Event Name: <Link to="/trip_page">Super Float Event</Link> Organizer: Jake A. Event Date: 8/15/25 - 8/17/25 Event Location: Bass River Resort</p>
+            <div className="Data">
+            {data.map((row, index) => (
+                <div key={index}>
+                    {row.map((cell, cellIndex) => (
+                        <span key={cellIndex}>{cell} </span>
+                    ))}
+                    </div>
+            ))}
+            </div>
         </div>
     );
 }
